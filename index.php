9460b537b70d8c8707db7d5ad929fd0e0b65a674
Nagy <janos@zold.lan>
2022-04-04 11:27:15 +0200

    PHP nyelv Konverzióig

--- index.php
+++ index.php
@@ -1,8 +1,49 @@
 <?php 
+$szam = 35.5;
+$nev = "János";
 
-echo file_get_contents('templates/head.tpl');
-echo file_get_contents('templates/nav.tpl');
-echo file_get_contents('templates/home.tpl');
-echo file_get_contents('templates/foot.tpl');
+echo "<!DOCTYPE html>\n";
+echo "<html>";
+echo "<body>";
+echo "alag\nsor<br>";
+echo "alag<br>sor";
+echo "<br> Típus: " . gettype($szam);
 
+echo <<<KISKUTYA
+<ul>
+    <li>alma</li>
+    <li>körte</li>
+</ul>
+KISKUTYA;
+
+echo "<br>PHP_OS: " . PHP_OS;
+const MIN_KOR=18;
+echo "<br>Jöhet: " . MIN_KOR;
+
+
+
+printf("<br>|%20d|", 58);
+
+//Matematikai függvények
+//Java, JavaScript Math.sqrt()
+//PHP
+echo "<br>";
+echo sqrt(9);
+
+echo "<br>";
+$szog = 1;
+echo sin($szog * pi() / 180);
+
+echo "<br>";
+echo pow(2, 8);
+
+
+echo "<br>";
+echo rand(1, 6);
+
+echo "<br>";
+$szamStr = "35";
+echo $szamStr + 2;
+
+echo "</body></html>";
 
