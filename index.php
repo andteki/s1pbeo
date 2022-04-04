8b7b58114d4da24d7ec16a7b1bd064d5e8966d53
Nagy <janos@zold.lan>
2022-04-04 09:46:40 +0200

    Kezdés

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo file_get_contents('templates/head.tpl');
+echo file_get_contents('templates/nav.tpl');
+echo file_get_contents('templates/home.tpl');
+echo file_get_contents('templates/foot.tpl');
+
+
---
+++ templates/home.tpl
@@ -0,0 +1,13 @@
+<p>
+Ez Beo nagykereskedés. 
+</p>
+
+<p>A Beo termékek organikus gazdaságból 
+    származnak.
+</p>
+<p>
+    Partnereink:
+    <ul>
+        <li>Micro</li>
+    </ul>
+</p>
